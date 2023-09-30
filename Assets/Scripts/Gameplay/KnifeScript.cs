1d5ccb5d9da89014714b19b0ee068d84d2a04167
Luke <eeveetrainer51@gmail.com>
2023-09-29 23:06:42 -0400

    Revert "fixes"
    This reverts commit a78f0b7cfc80e0868d953a04107fdea7637189c9.

--- Assets/Scripts/Gameplay/KnifeScript.cs
+++ Assets/Scripts/Gameplay/KnifeScript.cs
@@ -9,27 +9,17 @@ public class KnifeScript : MonoBehaviour
     private Touch touch;
     [SerializeField] GameObject knife;
     private Camera cam;
-<<<<<<< Updated upstream
 
-=======
-    private string inputs;
->>>>>>> Stashed changes
     private void Start(){
         cam = Camera.main;
     }
     void Update(){
     //Handles movement/location of swiping the knife
-<<<<<<< Updated upstream
     
-=======
-
-    //This is for mobile.
->>>>>>> Stashed changes
       if(Input.touchCount > 0){
         touch = Input.GetTouch(0);
         if(touch.phase == TouchPhase.Began){
             swiping = true;
-<<<<<<< Updated upstream
         }
         if(touch.phase == TouchPhase.Ended){
             swiping = false;
@@ -47,43 +37,5 @@ public class KnifeScript : MonoBehaviour
     private void OnTriggerEnter(Collider other){
         
 
-=======
-            inputs = "mobile";
-        }
-        if(touch.phase == TouchPhase.Ended){
-          swiping = false;
-        }
-      }
-
-        if(swiping){
-          if(inputs == "mobile"){
-            touchPos = touch.position;
-            touchPos.z = 3f;
-            
-          }
-
-          if(inputs == "pc"){
-            touchPos = Input.mousePosition;
-            touchPos.z = 3f;
-
-          }
-
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
-            
-            knife.transform.position = worldPos;
-        }
-    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-    //This is for PC
-    if(Input.GetMouseButtonDown(0)){
-      swiping = true;
-      inputs = "pc";
-    }
-
-    if(Input.GetMouseButtonUp(0)){
-      swiping = false;
-    }
- 
->>>>>>> Stashed changes
     }
 }
