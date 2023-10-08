3cdc4721f29513cd65e8ca2489e34140f0c72d35
Tommytippi <tavp040302@knights.ucf.edu>
2023-10-08 17:23:20 -0400

    Pause menu buttons finished

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BackToMenu : MonoBehaviour
+{
+    public void OpenLevel(int levelId)
+    {
+        string levelName = "Main Menu " + levelId;
+        SceneManager.LoadScene(levelName);
+    }
+}
+
